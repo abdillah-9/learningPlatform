f8920018d8b5376acd3ddf7003f0ed0cd2cae724
abdillah-9 <sabdillah855@gmail.com>
2025-12-28 22:01:31 +0300

    cool

--- src/components/ViewAllCourses.jsx
+++ src/components/ViewAllCourses.jsx
@@ -63,6 +63,7 @@ export default function ViewAllCourses(){
                     <div style={{ position: 'relative', width: '100%',height:'100%' }}>
                     <img
                         src={`https://www.tanzcoffee.co.tz/mwangaza-backend/uploads/`+course.picture}
+                        crossOrigin="anonymous"
                         alt="slide"
                         style={{ width: '100%', height: '100%', objectFit: 'cover' }}
                     />

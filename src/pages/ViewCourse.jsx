6557671110af35c97ac96d2cf5f24a4a60f4824b
abdillah-9 <sabdillah855@gmail.com>
2025-12-30 17:20:08 +0300

    cool

--- src/pages/ViewCourse.jsx
+++ src/pages/ViewCourse.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState, useMemo } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { AuthContext } from "../AuthProvider";
 import { RiFileList3Line } from "react-icons/ri";
+import { LucideClipboardList } from "lucide-react";
 
 const decodeHTML = (encoded) => {
   try {
@@ -182,11 +183,14 @@ export default function AAviewLastCourse() {
           <div style={{display:'flex', alignItems:'center', padding:'6px', gap:'6px',boxShadow:'1px 1px 20px rgba(100,100,100,0.6)', borderRadius:'5px', cursor:'pointer', width:'100%', maxWidth:'150px,',}}
           onClick={()=>{navigateTo('/view_all_courses', {replace:true})}}>
             <span style={{fontSize:'14px',backgroundColor:"#F4B342", borderRadius:'50%', display:'flex', alignItems:'center', justifyContent:'center', padding:'10px', color:"#684200ff"}}><RiFileList3Line/></span>
-            <span>View All Courses</span>
+            <span style={{fontSize:'13px'}}>View All Courses</span>
           </div>
           <span style={{display:'flex', alignItems:'center', padding:'6px', gap:'6px',boxShadow:'1px 1px 20px rgba(100,100,100,0.6)', borderRadius:'5px', cursor:'pointer', width:'100%', maxWidth:'200px', fontSize:'14px'}}
           onClick={()=>{navigateTo(`/enroll_course/${courseId}`, {replace:true})}}>
-            View other modules which are on the same course as
+            <span style={{fontSize:'14px',backgroundColor:"#F4B342", borderRadius:'50%', display:'flex', alignItems:'center', justifyContent:'center', padding:'10px', color:"#684200ff"}}><LucideClipboardList/></span>
+            <span style={{fontSize:'13px'}}>
+              View other modules which are on the same course as <strong>{currentModule?.title}</strong>
+            </span>
           </span>
         </div>
       </div>

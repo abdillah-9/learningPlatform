6bea5ab7d9a5f5cc2165caa16e4276ab58c84970
abdillah-9 <sabdillah855@gmail.com>
2026-01-02 18:26:51 +0300

    dash

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -163,7 +163,7 @@ export default function Course(){
                                     <div style={{paddingLeft:'30px', cursor:'pointer'}} onClick={()=>{navigateTo(`main_App/moduleId/${item.id}`)}}>
                                         <div style={{fontSize:'16px', fontWeight:500, display:'flex', width:'100%', justifyContent:'space-between', flexWrap:'wrap', gap:'12px'}}>
                                             <div>Module {++index}: {item.title}</div>
-                                            <div>Click Enroll Now to get started</div>
+                                            <div style={{fontSize:'13px', boxShadow:'1px 0.5px 6px rgba(52, 51, 51, 0.72)', color:'rgba(52, 51, 51, 0.72)'}}>Click Enroll Now to get started</div>
                                         </div>
                                         <p style={{color:'rgba(80,80,80,0.8)', maxWidth:'700px'}}>
                                             {item.description}

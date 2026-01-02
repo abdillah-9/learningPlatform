97424b2afd2bc4d29f502f3b70e420a15526bc5f
abdillah-9 <sabdillah855@gmail.com>
2026-01-02 11:01:23 +0300

    dash

--- src/features/admin/AvailableCourses.jsx
+++ src/features/admin/AvailableCourses.jsx
@@ -164,7 +164,7 @@ function ShowCourses({loading, setLoading, setCourses, courses ,fetchCourses ,se
       {courses?.map((course, courseIndex) => (
         <div key={course.id} style={{ marginBottom: "25px" }}>
           <h5 style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-            <span>{`Course ${courseIndex + 1}: ${course.name}`}</span>
+            <span style={{padding:'10px', borderRadius:'20px', width:'200px', border:'1px solid #F0B400', fontSize:'20px'}}>{`Course ${courseIndex + 1}: ${course.name}`}</span>
             <span style={{ display: "flex", gap: "10px" }}>
               <button
                 style={{

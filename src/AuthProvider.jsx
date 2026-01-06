9bb0c2674de55d4a80e9681c4b6571fb76be9727
abdillah-9 <sabdillah855@gmail.com>
2026-01-06 08:47:28 +0300

    shiit

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect, useState } from "react";
+import { IoMdHelpCircleOutline } from "react-icons/io";
 
 export const AuthContext = createContext();
 
@@ -19,7 +20,10 @@ export default function AuthProvider({ children }) {
   return (
     <AuthContext.Provider value={{ userData, setUserData, loading }}>
       {children}
-      <div style={{position:'fixed', right:'10px', bottom:'10px', borderRadius:'50px', backgroundColor:'rgba(0, 133, 117, 0.8)', color:'white', padding:'10px 12px', fontSize:'14px', cursor:'pointer',zIndex:10}}>Help</div>
+      <div style={{position:'fixed', right:'10px', bottom:'10px', borderRadius:'50px', backgroundColor:'rgba(0, 133, 117, 0.8)', color:'white', padding:'10px 12px', fontSize:'13px', cursor:'pointer',zIndex:10, display:'flex', gap:'4px', fontWeight:600, alignItems:'center'}}>
+        <IoMdHelpCircleOutline style={{fontSize:'15px'}}/>
+        <span>Help</span>
+      </div>
     </AuthContext.Provider>
   );
 }

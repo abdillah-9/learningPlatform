d088f2e8677a1def5efdc13b233b94b827d02ea7
abdillah-9 <sabdillah855@gmail.com>
2026-01-07 12:21:40 +0300

    shiit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,7 +28,7 @@ export default function About(){
     </div>
 
     {/** Huge Image */}
-    <div style={{height:'calc(100vh)',width:'100vw', position:'relative', overflow:'hidden',}}>
+    <div style={{height:'auto',width:'100vw', position:'relative', overflow:'hidden',aspectRatio:16/9}}>
       <img src={aboutImage} alt='pic' width={'100%'} height={'auto'} style={{aspectRatio:16/9}}/>
       <div style={{position:'absolute', top:0, left:0, width:'100%', height:'100%',backgroundColor:'rgba(1, 1, 54, 0.35)', color:'white', display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column', gap:'50px', textAlign:'center', padding:'10px'}}>
         <h1 style={{fontSize:'40px', fontWeight:800}}>About Us</h1>

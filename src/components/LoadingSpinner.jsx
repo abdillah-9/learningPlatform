891899ba6bea6c2e68e761c26db3d961efcaf31b
abdillah-9 <sabdillah855@gmail.com>
2026-01-08 07:26:28 +0300

    shiit

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -5,7 +5,7 @@ import { VscLoading } from 'react-icons/vsc'
 
 export default function LoadingSpinner() {
   return (
-    <div className='centered flex-Column' style={{height:"100vh", width:"100vw", color:"#F4B342"}}>
+    <div className='centered flex-Column' style={{height:"100vh", width:"100vw", color:"#FAA533"}}>
       <div className='loadingSpinner'>
         <TbLoader3 style={{fontSize:'100px'}}/>
       </div>
--- src/components/MiniLoadingSpinner.jsx
+++ src/components/MiniLoadingSpinner.jsx
@@ -5,7 +5,7 @@ import { VscLoading } from 'react-icons/vsc'
 
 export default function MiniLoadingSpinner() {
   return (
-    <div className='centered flex-Column' style={{height:"100%", width:"100%", color:"#F4B342"}}>
+    <div className='centered flex-Column' style={{height:"100%", width:"100%", color:"#FAA533"}}>
       <div className='loadingSpinner'>
         <TbLoader3 style={{fontSize:'100px'}}/>
       </div>

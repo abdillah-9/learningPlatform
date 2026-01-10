d3304705582c4ebc69f674b27d2a2d0c74aabb33
abdillah-9 <sabdillah855@gmail.com>
2026-01-10 14:56:01 +0300

    shiit

--- src/features/admin/CourseManager.jsx
+++ src/features/admin/CourseManager.jsx
@@ -409,6 +409,7 @@ export default function CourseManager({ formState, setFormState, editModeState, 
                 style={input}
               />
 
+              {console.log(module.desc)}
               <label>Module Description</label>
               <textarea
                 rows={2}

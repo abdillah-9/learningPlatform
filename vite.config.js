eb478af375f70a110f74348a17ecbd262e0e83df
abdillah-9 <sabdillah855@gmail.com>
2026-01-14 12:28:08 +0300

    shiit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/mwangaza_hub/', // IMPORTANT: add your subdirectory here
+  plugins: [react()]
 })
